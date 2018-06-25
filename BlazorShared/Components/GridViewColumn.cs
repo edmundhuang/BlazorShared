43978b1a3914d5ea772cd6bebe096a09b9fdbaa0
edmundhuang <changchun.huang@gmail.com>
2018-06-25 08:40:22 +0800

    Column sort 功能

--- BlazorShared/Components/GridViewColumn.cs
+++ BlazorShared/Components/GridViewColumn.cs
@@ -8,6 +8,8 @@ namespace BlazorShared.Components
         public string DisplayName { get; set; }
         public PropertyInfo Property { get; set; }
 
+        public bool Sortable { get; set; }
+
         public bool isExtra { get; set; } = false;
         public string CellValue { get; set; }
 
